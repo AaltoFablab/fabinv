b5b00e038e9c7bf0f07193d7881431a7737bc152
laqbrask <lauri.brask@aalto.fi>
2019-06-19 14:44:54 +0300

    Added route to index.html

--- backend/app.py
+++ backend/app.py
@@ -1,15 +1,19 @@
 from database import init_db
-from flask import Flask
+from flask import Flask, render_template
 from flask_graphql import GraphQLView
 from schema import schema
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='../frontend')
 app.debug = True
 
 app.add_url_rule(
     '/graphql',
     view_func=GraphQLView.as_view('graphql', schema=schema, graphiql=True)
 )
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     init_db()
