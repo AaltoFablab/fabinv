d78f727fd5e09a9c7b4d80bf61d4f1a1da9b63a3
laqbrask <lauri.brask@aalto.fi>
2019-06-25 13:40:12 +0300

    Add static file handling to app.py

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,9 @@
 from database import init_db
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 from flask_graphql import GraphQLView
 from schema import schema
 
-app = Flask(__name__, template_folder='../frontend')
+app = Flask(__name__, template_folder='../frontend', static_url_path='', static_folder='../frontend')
 app.debug = True
 
 app.add_url_rule(
